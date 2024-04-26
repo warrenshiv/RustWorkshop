6c7a80c33574f07fede5a2c6147755f963d3b005
warrenshiv <warrenshiv@gmail.com>
2024-04-26 22:41:17 +0300

    Add Session1 hello-world project files

---
+++ Session1/hello-world/target/debug/deps/hello_world-217e24ebed2e27a7.d
@@ -0,0 +1,5 @@
+/home/warren/Projects/RustWorkshop/Session1/hello-world/target/debug/deps/hello_world-217e24ebed2e27a7: src/main.rs
+
+/home/warren/Projects/RustWorkshop/Session1/hello-world/target/debug/deps/hello_world-217e24ebed2e27a7.d: src/main.rs
+
+src/main.rs:
---
+++ Session1/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/home/warren/Projects/RustWorkshop/Session1/hello-world/target/debug/hello-world: /home/warren/Projects/RustWorkshop/Session1/hello-world/src/main.rs
