c3a75b491579f60e2057c1ce029055adb78c6128
warrenshiv <warrenshiv@gmail.com>
2024-05-16 11:57:37 +0300

    Updates

---
+++ assignment1/target/debug/assignment1.d
@@ -0,0 +1,1 @@
+/home/warren/RustWorkshop/assignment1/target/debug/assignment1: /home/warren/RustWorkshop/assignment1/src/main.rs
---
+++ assignment1/target/debug/deps/assignment1-5cc74abd15547885.d
@@ -0,0 +1,5 @@
+/home/warren/RustWorkshop/assignment1/target/debug/deps/assignment1-5cc74abd15547885: src/main.rs
+
+/home/warren/RustWorkshop/assignment1/target/debug/deps/assignment1-5cc74abd15547885.d: src/main.rs
+
+src/main.rs:
